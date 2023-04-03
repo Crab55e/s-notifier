6df7cfc5c81e60c25a53b6be6bedc8a9a373af6a
Crab55e <99100129+Crab55e@users.noreply.github.com>
2023-04-03 16:58:01 +0900

    ミスってアップロードしたやつを削除

--- program.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-from time import sleep
-while True:
-    for i in range(25):
-        if len(str(i)) == 1:
-            ctypes.windll.user32.SystemParametersInfoW(20, 0, f"C:/Users/crab_/OneDrive/デスクトップ/result/myBGAnim0{i}.png" , 0)
-        else:
-            ctypes.windll.user32.SystemParametersInfoW(20, 0, f"C:/Users/crab_/OneDrive/デスクトップ/result/myBGAnim{i}.png" , 0)
-        sleep(0.1)
